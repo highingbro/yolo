37fdf0ef9abad187fe3da6ad0fa26aa7018932af
highingbro <jiang.2639@osu.edu>
2025-06-23 14:35:57 +0800

    Update video.py

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 import cv2
 capture = cv2.VideoCapture(6)
 
-# 定义编码方式并创建 VideoWriter 对象
+
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 outfile = cv2.VideoWriter('output.mp4', fourcc, 25., (640, 480))
 
@@ -9,7 +9,7 @@ while(capture.isOpened()):
     ret, frame = capture.read()
 
     if ret:
-        outfile.write(frame)  # 写入文件
+        outfile.write(frame)  
         cv2.imshow('frame', frame)
         
         if cv2.waitKey(1) == ord('q'):
